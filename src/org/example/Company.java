28fd195c99592d0dfeaa48f3fcdb6143c6dcb740
Shahanameet <meet2shahana@gmail.com>
2022-01-08 16:32:47 +0530

    b done the work

--- src/org/example/Company.java
+++ src/org/example/Company.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Company {
+	private void m2() {
+		System.out.println("method created by B");
+	}
 	public static void main(String[] args) {
 		int a=100;
 		System.out.println(a);
