082341486920c808dbce6ecb93c438cbd1667395
Shahanameet <meet2shahana@gmail.com>
2022-01-08 17:05:40 +0530

    C done the work

--- src/org/example/Company.java
+++ src/org/example/Company.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Company {
+	private void m3() {
+		System.out.println("m3 created by C branch");
+	}
 	public static void main(String[] args) {
 		int a=100;
 		System.out.println(a);
